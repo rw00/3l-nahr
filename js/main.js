c45b671ebbb3f25b013547af44d6cf917f3f25da
rw00 <raafatwahb@gmail.com>
2024-02-19 23:01:49 +0100

    Initial commit sereno

---
+++ js/main.js
@@ -0,0 +1,70 @@
+const HASH_SEPARATOR = "#";
+const EN_LANG = "en";
+const AR_LANG = "ar";
+
+$(document).ready(function () {
+  let lang = resolveLanguage();
+  showTextInLanguage(lang);
+  let $langSelector = $("#lang-selector");
+  $langSelector.on("change", function (e) {
+    let selectedLang = $langSelector.val();
+    showTextInLanguage(selectedLang);
+  });
+
+  initScroll();
+});
+
+function initScroll() {
+  let controller = new ScrollMagic.Controller();
+
+  new ScrollMagic.Scene({
+    duration: "200%",
+    triggerHook: "onEnter"
+  })
+    .setPin(".parallax")
+    .addTo(controller);
+}
+
+function showTextInLanguage(lang) {
+  setLanguageInUrl(lang);
+  $(".txt").each((_, e) => {
+    if (e.id) {
+      if (lang === EN_LANG || lang === AR_LANG) {
+        let obj = eval(lang);
+        let translation = obj[e.id];
+        if (translation) {
+          e.innerHTML = translation;
+          if (lang === AR_LANG) {
+            e.classList.add(AR_LANG + "-txt");
+          } else {
+            e.classList.remove(AR_LANG + "-txt");
+          }
+        }
+      }
+    }
+  });
+}
+
+function resolveLanguage() {
+  let url = $(location).attr("href");
+  let index = url.indexOf(HASH_SEPARATOR);
+  if (index > 0) {
+    let lang = url.substring(index);
+    if (lang === AR_LANG) {
+      return AR_LANG;
+    }
+  }
+  return EN_LANG; // default
+}
+
+function setLanguageInUrl(lang) {
+  let url = $(location).attr("href");
+  let base;
+  let index = url.indexOf(HASH_SEPARATOR);
+  if (index > 0) {
+    base = url.substring(0, index);
+  } else {
+    base = url;
+  }
+  $(location).attr("href", base + HASH_SEPARATOR + lang);
+}
---
+++ js/text/en.js
@@ -0,0 +1,13 @@
+const en = {
+  "weddings-venue": "Weddings Venue",
+  "coming-this-summer-venue": "Coming this Summer: a beautiful venue for your special day!",
+  "sereno-venue-description": "Sereno offers a large venue that accommodates all your friends and family",
+  "sereno-venue-description-more": "We can arrange catering and music to your taste and like.",
+  "park-headline": "A Park for Good Times",
+  "park-more-info": "Enjoy the Summer days at Sereno, chilling and barbecuing with friends and family.",
+  "contact-us": "Contact us",
+  "contact-details-description":
+    "We're happy to answer your questions and take your reservations! <br />You can reach out to us via",
+  phone: "Phone",
+  email: "Email"
+};
